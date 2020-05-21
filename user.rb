502246df408518ac0eb27f564d25434e5c68b183
inaba <kuchibuefuite@icloud.com>
2020-05-21 19:28:27 +0900

    課題validates

---
+++ user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { in:1..15}
+  validates :email, presence: true
+  validates :email, format: { with: /\A\S+@\S+¥.\S+\z/ }
+  validates :password, length: { in: 8..32 }
+  validates :password, format: { with: /\A[a-z0-9]+\z/ }
+
+  has_secure_password
+end
